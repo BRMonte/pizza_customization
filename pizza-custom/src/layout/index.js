1c00eeeebdbe917051bd137a1bb9d03c1ee0b4ee
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-10 22:39:05 +0000

    Install React Router

--- pizza-custom/src/layout/index.js
+++ pizza-custom/src/layout/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const Layout = (props) => (
-  <div>
-  <nav>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>
-  {props.children}
+  <div className="bg-purple-500 border border-black m-5">
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    {props.children}
   </div>
 );
 
